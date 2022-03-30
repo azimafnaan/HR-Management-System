f23a62ccfeffa3317695c1bb7a23d61909f47803
azimafnaan <azim.afnaan@hotmail.com>
2022-03-31 03:40:47 +0600

    Update Employee Edit Section

--- src/components/HR/Employee/Employee.js
+++ src/components/HR/Employee/Employee.js
@@ -16,50 +16,23 @@ const Employee = () => {
     const myEmployee = employee ? employee.find(td => td.id == employeeId) : []
     return (
         <div className="employee">
-            <form onSubmit={handleSubmit(onSubmit)}>
-
-
-                <input defaultValue={myEmployee?.id} type="number" {...register("id", { required: true, maxLength: 50 })} placeholder="Employee ID" />
-
-
-
-                <input defaultValue={myEmployee?.firstName} {...register("firstName", { maxLength: 50 })} placeholder="First Name" />
-
-                <input defaultValue={myEmployee?.lastName} {...register("lastName", { maxLength: 50 })} placeholder="Last Name" />
-
-
-                <input defaultValue={myEmployee?.email} type="email" {...register("email", { required: true, maxLength: 50 })} placeholder="Email" />
-
-
+            <form className="employeeEditForm" onSubmit={handleSubmit(onSubmit)}>
+                <input defaultValue={myEmployee?.id} {...register("id")} placeholder="Employee ID" />
+                <input defaultValue={myEmployee?.firstName} {...register("firstName")} placeholder="First Name" />
+                <input defaultValue={myEmployee?.lastName} {...register("lastName")} placeholder="Last Name" />
+                <input defaultValue={myEmployee?.email} {...register("email")} placeholder="Email" type="email" />
                 <input defaultValue={myEmployee?.contactNumber} {...register("contactNumber")} placeholder="Contact Number" />
-
-
-                <input defaultValue={myEmployee?.deportment} {...register("deportment", { required: true, maxLength: 50 })} placeholder="Deportment" />
-
-                <input defaultValue={myEmployee?.position} {...register("position", { required: true, maxLength: 50 })} placeholder="Position" />
-
-
-                <input defaultValue={myEmployee?.joiningDate}  {...register("joiningDate", { required: true, maxLength: 50 })} placeholder="Joining Date" />
-
-
+                <input defaultValue={myEmployee?.deportment} {...register("deportment")} placeholder="Deportment" />
+                <input defaultValue={myEmployee?.position} {...register("position")} placeholder="Position" />
+                <input defaultValue={myEmployee?.joiningDate} {...register("joiningDate")} placeholder="Joining Date" />
                 <input defaultValue={myEmployee?.salary} {...register("salary")} placeholder="Salary" />
+                <input defaultValue={myEmployee?.accountNumber} {...register("accountNumber")} placeholder="Account Number" />
+                <input defaultValue={myEmployee?.dateOfBirth} {...register("dateOfBirth")} placeholder="Date Of Birth" />
+                <input defaultValue={myEmployee?.age} {...register("age")} placeholder="Age" />
+                <input defaultValue={myEmployee?.gender} {...register("gender")} placeholder="Gender" />
 
-                <input defaultValue={myEmployee?.accountNumber} type="number" {...register("accountNumber", { required: true, maxLength: 50 })} placeholder="Account Number" />
-
-
-                <input defaultValue={myEmployee?.dateOfBirth}  {...register("dateOfBirth", { required: true, maxLength: 50 })} placeholder="Date Of Birth" />
-
-
-                <input defaultValue={myEmployee?.age} type="number" {...register("age", { required: true, maxLength: 50 })} placeholder="Age" />
-
-
-
-                <input defaultValue={myEmployee?.gender}  {...register("gender", { required: true, maxLength: 50 })} placeholder="Gender" />
-
-                <input type="submit" />
-
+                <input type="submit" className="submit-btn" />
             </form>
-
         </div>
     );
 };
